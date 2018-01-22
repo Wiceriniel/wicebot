172427c1b8deb53b065564f3e457020a0cc84f1b
Wiceriniel <31278081+Wiceriniel@users.noreply.github.com>
2018-01-23 02:42:14 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+client.on('ready', () => {
+  console.log('Process started.');
+});
+ 
+client.on('message', message => {
+  
+  switch(message.content) {
+
+    case 'ping':
+    message.channel.send('Pong!')
+        break;
+
+    case 'sa':
+    message.reply('as')
+        break;
+
+    case 'hi':
+    message.channel.send('hey')
+        break;
+
+    default:
+        console.log(message.content)
+}
+
+});
+ 
+client.login(process.env.BOT_TOKEN);
