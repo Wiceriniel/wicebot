36324df73ec4698a551ead205ee1bf3bf0e396a1
Onur Akkuş <31278081+Wiceriniel@users.noreply.github.com>
2018-06-23 18:42:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,102 +1,98 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+var prefix = "+";
  
 client.login(process.env.BOT_TOKEN);
 
 client.on('ready', () => {
-  console.log('Process started.');
+  console.log('Wicebot is online!');
 
-  client.user.setGame('$help');
-
-});
- 
-client.on('message', message => {
-
-  //message.channel.send('message')
-
-  switch(message.content) {
-
-    case "$help":
-    message.reply("This is a chatbot. Type **.message** to start a conversation.")
-      break;
-
-    case 'ping':
-    message.reply('Pong!')
-        break;
-
-    case 'sa':
-    message.reply('as')
-        break;
-    
-   case 'Sa':
-    message.reply('as')
-        break;
-    
-    case 'SA':
-    message.reply('as')
-        break;
-
-    case 'hi':
-    message.reply('hey')
-        break;
-
-    default:
-
-
-}
-
-// DEVELOPER COMMANDS
-
-var me = process.env.ME_I;
-
-if((message.cleanContent.startsWith("$") || message.channel.type == 'dm') && message.author.id == me){
-
-  var cmd = message.content.split("$");
-
-
-  switch(cmd[1]) {
-
-    case "me":
-    message.reply(message.author.id);
-      break;
-
-    case "setnick":
-    message.guild.members.get(client.user.id).setNickname(cmd[2]);
-    message.reply("Nickname is set to " + cmd[2] + ".");
-      break;
-
-    case "setstatus":
-    client.user.setGame(cmd[2]);
-    message.reply("Status is set to " + cmd[2] + ".");
-      break;
-
-    case "movemember":
-    const channel = message.guild.channels.find('id', cmd[2]);
-    //const membertomove = message.guild.members.get(cmd[2]);
-    message.member.setVoiceChannel(channel);
-    //message.delete();
-        break;
-
-    case "setvc":
-    const user = message.mentions.members.first();
-    message.guild.member(user).setVoiceChannel(message.guild.channels.find("id", cmd[2]));
-    message.delete();
-      break;
-      
-    case "c":
-    const channel2 = message.guild.channels.find('id', process.env.VOICE_GYK);
-    message.member.setVoiceChannel(channel2);
-    message.delete();
-      break;
-
-    default:
-    if(message.content === "$help") {} else {
-    message.reply("$" + cmd[1] + " : Unknown command."); }
-}
-
-}
-
-
+  client.user.setActivity('+komutlar');
 
 });
- 
+
+
+client.on('message', async message => {
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  
+  if(message.content === 'sa' || message.content === 'Sa' || message.content === 'SA' || message.content === 'sA') {
+    message.reply('as');
+  } else
+  if (command === 'ping') {
+    const m = await message.channel.send("Gecikme hesaplanıyor...");
+    m.edit(`Gecikme ${m.createdTimestamp - message.createdTimestamp}ms. API gecikmesi ${Math.round(client.ping)}ms.`);
+  } else
+  if (command === 'kontrol') {
+    message.channel.send("**Prefix: **" + prefix + "\n**Command: **" + command + "\n**Arguments: **" + args);
+  } else
+  if (command === 'alıntı') {
+    
+    message.channel.fetchMessage(args[0])
+  .then(msg => mesajYolla(msg.content, msg.author.username, msg.author.discriminator, msg.author.avatarURL, msg.createdAt, msg.editedAt))
+  .catch(console.error);
+
+  function mesajYolla(icerik, ad, tag, avatar, olusturmaZamani, editZamani) {
+    var zaman;
+    if (editZamani == null) {
+      zaman = olusturmaZamani;
+    } else {
+      zaman = editZamani;
+    }
+
+    message.channel.send("Kanal: <#" + message.channel.id + "> | Alıntılayan: <@" + message.author.id + ">");
+    message.channel.send({
+      "embed": {
+
+      "description": icerik,
+
+      "color": 3381759,
+      "timestamp": zaman,
+
+      /*"image": {
+        "url": "https://cdn.discordapp.com/attachments/425945005635665922/459843736046796810/ba74954dde74ff40a32ff58069e78c36.png"
+      },*/
+
+      "author": {
+        "name": ad + "#" + tag,
+        //"url": "https://discordapp.com",
+        "icon_url": avatar
+      }
+    }});
+  }
+  
+  message.delete();
+
+  } else
+
+  if (command === "komutlar") {
+      message.channel.send("**WiceBot komutları: **\n\n**" + prefix + "yardım komut_adı** : Verilen komutu detaylı açıklar. Örnek: " + prefix + "yardım alıntı\n**" + prefix +"alıntı mesaj_id** : ID'si verilen mesajı alıntılar.\n**" + prefix + "ping:** Gecikmeyi hesaplar.");
+  } else
+
+  if (command === "pin") {
+    message.channel.fetchMessage(args[0])
+  .then(msg => msg.pin())
+  .catch(console.error);
+  }
+
+  if (command === "delete") {
+    message.channel.fetchMessage(args[0])
+  .then(msg => msg.delete())
+  .catch(console.error);
+  message.delete();
+  } else
+
+  if (command === "yardım") {
+    if (args[0] === "alıntı") {
+      message.channel.send("https://www.youtube.com/watch?v=9nwYFrLZvYw");
+    } else {
+      message.channel.send("Geçersiz komut.");
+    }
+  }
+
+});
+
+//"2018-06-22T20:08:34.503Z"
